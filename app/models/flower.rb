9e5f394e672e511aef9762ab630cffcbee53ad87
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-12-04 21:07:40 +0300

    add flower resoource

---
+++ app/models/flower.rb
@@ -0,0 +1,4 @@
+class Flower < ApplicationRecord
+    has_many :reviews
+    has_many :users, through: :reviews
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :flowers
   resources :reviews, only: [:index, :create, :update, :delete]
   post "/signup", to: "users#create"
   get "me", to: "users#show"
